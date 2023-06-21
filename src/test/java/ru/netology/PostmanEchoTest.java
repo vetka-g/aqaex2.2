61f0833c2964e59ac40418877636e51b4f2911fb
Sveta Golubeva <veta.golubeva@gmail.com>
2023-06-21 21:50:20 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,20 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void ShouldReturnSendData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello World!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hello World!"));
+    }
+}
